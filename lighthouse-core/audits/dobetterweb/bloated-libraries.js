e5d318a1e81ddc0f30889d8c726a00f669b0d745
Saavan Nanavati <saavan.nanavati@utexas.edu>
2020-07-14 11:48:45 -0500

    Fix UIStrings, type comments, and inline searchBloatedDatabase

--- lighthouse-core/audits/dobetterweb/bloated-libraries.js
+++ lighthouse-core/audits/dobetterweb/bloated-libraries.js
@@ -15,15 +15,18 @@ const Audit = require('../audit.js');
 const i18n = require('../../lib/i18n/i18n.js');
 
 const UIStrings = {
-  /** Title of a Lighthouse audit that provides detail on the Javascript libraries that are used on the page. */
-  title: 'Avoid bloated JavaScript dependencies',
-  /** Description of a Lighthouse audit that tells the user what this audit is detecting. This is displayed after a user expands the section to see more. No character length limits. */
-  description: 'These are suggestions for functionally equivalent library alternatives ' +
-    'that can reduce your bundle size. [Learn more](https://web.dev/bloated-libraries/)',
+  /** Descriptive title of a Lighthouse audit that provides detail on the large Javascript libraries that are used on the page that have better alternatives. */
+  title: 'Avoid unnecessarily large JavaScript dependencies',
+  /** Description of a Lighthouse audit that tells the user why they should care about the large Javascript libraries that have better alternatives.. This is displayed after a user expands the section to see more. No character length limits. */
+  description: 'Large JavaScript libraries can lead to poor performance. ' +
+    'Prefer smaller, functionally equivalent libraries to reduce your bundle size.' +
+    '[Learn more](https://developers.google.com/web/fundamentals/performance/webpack/decrease-frontend-size#optimize_dependencies',
+  /** Label for a column in a data table. Entries will be names of suggested libraries that are an alternative to what's currently being used. */
   suggestion: 'Suggestion',
 };
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
+const database = {'moment': 'dayjs'};
 
 class BloatedLibrariesAudit extends Audit {
   /**
@@ -39,27 +42,18 @@ class BloatedLibrariesAudit extends Audit {
   }
 
   /**
-   * If the given library is bloated, adds a library/suggestion pairing to libraryPairings.
-   * @param {LH.Artifacts.DetectedStack} library
-   * @param {object[]} libraryPairings
-   */
-  static searchBloatedDatabase(library, libraryPairings) {
-    const database = {'moment': 'dayjs'};
-
-    if (library.npm && database[library.npm]) {
-      libraryPairings.push({original: library, suggestion: {name: database[library.npm]}});
-    }
-  }
-
-  /**
    * @param {LH.Artifacts} artifacts
    * @return {LH.Audit.Product}
    */
   static audit(artifacts) {
+    /** @type {Array<{original: {name: string}, suggestion: {name: string}}>} */
     const libraryPairings = [];
-
     const foundLibraries = artifacts.Stacks.filter(stack => stack.detector === 'js');
-    foundLibraries.forEach(library => this.searchBloatedDatabase(library, libraryPairings));
+
+    for (const library of foundLibraries) {
+      if (!library.npm || !database[library.npm]) continue;
+      libraryPairings.push({original: library, suggestion: {name: database[library.npm]}});
+    }
 
     const tableDetails = [];
     libraryPairings.forEach(libraryPairing => {
